d6d4c956574865e48f6698363551a4050547f3ba
gnit24 <geeuinit@gmail.com>
2024-05-21 04:14:12 -0500

    update Script with better detailed output

--- PySpeedTester.py
+++ PySpeedTester.py
@@ -3,17 +3,25 @@ import speedtest
 def bytes_to_mb(bytes):
     KB = 1024
     MB = KB * 1024
-    return int(bytes/MB)
+    return round(float(bytes/MB),2)
 
 st = speedtest.Speedtest()
 
-start = input("Press Enter to Start")
+start = input("Press Enter to Start: ")
+st.get_best_server()
 down_speed = str(bytes_to_mb(st.download()))
 up_speed = str(bytes_to_mb(st.upload()))
 ping = int(st.results.ping)
 
+server = st.results.server
+server_name = server['sponsor']
+server_location = server['name']
+server_country = server['country']
+
 print("------------------------------")
-print('D/L:  ' + down_speed + 'mb/s')
-print('U/L:  ' + up_speed + 'mb/s')
+print(f'Server: {server_name}, {server_location}, {server_country}')
+print('D/L:  ' +  down_speed + ' Mbps')
+print('U/L:  ' + up_speed + ' Mbps')
 print(f"Ping: {ping} ms")
 print("------------------------------")
+
